848e50a702a67f5c369eed31bcc3cd969f437f35
Dean Marano <here@dean.is>
2026-03-28 22:17:24 -0400

    fix: export NullColumn, SchemaReflection, BoundSchemaReflection, FakePool, Deduplicable

--- packages/activerecord/src/connection-adapters.ts
+++ packages/activerecord/src/connection-adapters.ts
@@ -12,9 +12,15 @@ export { ConnectionHandler } from "./connection-adapters/abstract/connection-han
 export { ConnectionPool } from "./connection-adapters/abstract/connection-pool.js";
 export { SchemaStatements } from "./connection-adapters/abstract/schema-statements.js";
 export { SchemaCreation } from "./connection-adapters/abstract/schema-creation.js";
-export { Column } from "./connection-adapters/column.js";
+export { Column, NullColumn } from "./connection-adapters/column.js";
 export { PoolConfig } from "./connection-adapters/pool-config.js";
 export { PoolManager } from "./connection-adapters/pool-manager.js";
-export { SchemaCache } from "./connection-adapters/schema-cache.js";
+export {
+  SchemaCache,
+  SchemaReflection,
+  BoundSchemaReflection,
+  FakePool,
+} from "./connection-adapters/schema-cache.js";
 export { SqlTypeMetadata } from "./connection-adapters/sql-type-metadata.js";
 export { StatementPool } from "./connection-adapters/statement-pool.js";
+export { deduplicate, type Deduplicable } from "./connection-adapters/deduplicable.js";

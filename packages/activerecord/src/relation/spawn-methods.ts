592380dad5dc458fc727f0fa465cef6b47fd372b
Dean Marano <here@dean.is>
2026-03-29 13:52:11 -0400

    fix: self-referential generic on SpawnRelation, type other as any in merge

--- packages/activerecord/src/relation/spawn-methods.ts
+++ packages/activerecord/src/relation/spawn-methods.ts
@@ -6,8 +6,8 @@
 
 import { Merger } from "./merger.js";
 
-interface SpawnRelation {
-  _clone(): SpawnRelation;
+interface SpawnRelation<T = unknown> {
+  _clone(): T;
 }
 
 /**
@@ -15,15 +15,17 @@ interface SpawnRelation {
  *
  * Mirrors: ActiveRecord::SpawnMethods#spawn
  */
-export function performSpawn<T extends SpawnRelation>(this: T): T {
-  return this._clone() as T;
+export function performSpawn<T extends SpawnRelation<T>>(this: T): T {
+  return this._clone();
 }
 
 /**
  * Merge another relation's conditions into this one.
+ * The `other` parameter is typed as `any` because Merger reads
+ * many Relation internals that aren't part of SpawnRelation.
  *
  * Mirrors: ActiveRecord::SpawnMethods#merge
  */
-export function performMerge<T extends SpawnRelation>(this: T, other: SpawnRelation): T {
+export function performMerge<T extends SpawnRelation<T>>(this: T, other: any): T {
   return new Merger(this, other).merge() as T;
 }

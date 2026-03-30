f649d85c66369e39a4f1e9444140675cf7b6b491
Dean Marano <here@dean.is>
2026-03-30 16:32:01 -0400

    fix: hoist xmlNameEscape regex to module scope
    Avoids per-call RegExp allocation in a hot path (tag/attribute building).

--- packages/activesupport/src/core-ext/string/output-safety.ts
+++ packages/activesupport/src/core-ext/string/output-safety.ts
@@ -47,19 +47,19 @@ export function htmlEscapeOnce(str: string): SafeBuffer {
  * Note: supplementary-plane code points (U+10000+) are not handled and will
  * be replaced with '_'. This covers all practical HTML/XML attribute names.
  */
+/* eslint-disable no-misleading-character-class -- XML spec character ranges */
+const XML_NAME_START =
+  /[@:A-Z_a-z\xC0-\xD6\xD8-\xF6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
+const XML_NAME_FOLLOWING =
+  /[@:A-Z_a-z\xC0-\xD6\xD8-\xF6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\xB7\u0300-\u036F\u203F-\u2040]/;
+/* eslint-enable no-misleading-character-class */
+
 export function xmlNameEscape(name: string): string {
   if (!name || name.length === 0) return "";
-  // Start character: @, :, A-Z, _, a-z, \xC0-\xD6, \xD8-\xF6, \xF8-\u02FF, etc.
-  /* eslint-disable no-misleading-character-class -- XML spec character ranges */
-  const VALID_START =
-    /[@:A-Z_a-z\xC0-\xD6\xD8-\xF6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD]/;
-  const VALID_FOLLOWING =
-    /[@:A-Z_a-z\xC0-\xD6\xD8-\xF6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\-.0-9\xB7\u0300-\u036F\u203F-\u2040]/;
-  /* eslint-enable no-misleading-character-class */
 
-  const chars: string[] = [VALID_START.test(name[0]) ? name[0] : "_"];
+  const chars: string[] = [XML_NAME_START.test(name[0]) ? name[0] : "_"];
   for (let i = 1; i < name.length; i++) {
-    chars.push(VALID_FOLLOWING.test(name[i]) ? name[i] : "_");
+    chars.push(XML_NAME_FOLLOWING.test(name[i]) ? name[i] : "_");
   }
   return chars.join("");
 }

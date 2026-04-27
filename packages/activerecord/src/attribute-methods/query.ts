eaaaf1b0be032ce48c593008375b064c64d8e118
Dean Marano <here@dean.is>
2026-04-27 12:31:05 -0400

    fix(activerecord): queryCastAttribute cast to BooleanType for dep lint

--- packages/activerecord/src/attribute-methods/query.ts
+++ packages/activerecord/src/attribute-methods/query.ts
@@ -71,6 +71,6 @@ function castToBoolean(value: unknown): boolean {
 
 // Mirrors: ActiveRecord::AttributeMethods::Query::ClassMethods private#query_cast_attribute
 function queryCastAttribute(this: any, attrName: string, value: unknown): unknown {
-  const type = this.typeForAttribute?.(attrName) ?? booleanType;
+  const type = (this.typeForAttribute?.(attrName) ?? booleanType) as BooleanType;
   return type.deserialize(value);
 }

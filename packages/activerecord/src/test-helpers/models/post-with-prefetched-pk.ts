8f101dab0e820013886b646255181fb310b0c602
Dean Marano <here@dean.is>
2026-05-23 16:18:58 -0400

    fix(test-models): widen nextSequenceValue return type, drop (Base as any) in PostWithPrefetchedPk

--- packages/activerecord/src/test-helpers/models/post-with-prefetched-pk.ts
+++ packages/activerecord/src/test-helpers/models/post-with-prefetched-pk.ts
@@ -1,7 +1,7 @@
 // vendor/rails/activerecord/test/models/post_with_prefetched_pk.rb
 import { Base } from "../../base.js";
 
-export class PostWithPrefetchedPk extends (Base as any) {
+export class PostWithPrefetchedPk extends Base {
   static _tableName = "posts";
 
   static isPrefetchPrimaryKey(): boolean {
